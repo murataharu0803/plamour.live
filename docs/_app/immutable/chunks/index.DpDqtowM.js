6cb266f4b8301334c485a14ad0a08f6e3eff5dd7
Harlos <cces4415@gmail.com>
2024-07-20 23:36:54 +0800

    build

---
+++ docs/_app/immutable/chunks/index.DpDqtowM.js
@@ -0,0 +1,1 @@
+const o=async a=>await(await fetch(`https://cloud.plamour.club/data/12F00liFHAf4rvB6N7ZqjxdRyVAgA444bMzPEvyhr3Q8/${a}`)).json();export{o as f};
---
+++ docs/_app/immutable/entry/start.CIl2VFEn.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.I9Wkjf8Y.js";export{t as start};
